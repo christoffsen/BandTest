81a48c3c2d0143049c0e9304d756139ce7ec987c
christoffsen <christopherelder@outlook.com>
2020-08-14 23:45:12 -0700

    VIDEO EMBEDDING AUGH

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-	<h1 style="font-family: 'Pacifico', cursive;">Some bullshit</h1>
+	<h1 style="font-family: 'Pacifico', cursive;">Roadtrippers</h1>
+	<h2>Locomotive Breath</h2>
+	<iframe src="https://drive.google.com/file/d/1zXpsRa5PXEbYV6BHgx-iCwLJOXYGNPA_/preview" width="640" height="480"></iframe>
      <div class="bottom-bar"/>
   </div>
 </template>
